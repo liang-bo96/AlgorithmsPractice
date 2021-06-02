e371125e65bab7f87c18aa75d5368f780f4f2782
liang-bo96 <67692596+liang-bo96@users.noreply.github.com>
2021-06-02 21:06:24 +0800

    6-2

--- src/com/company/SubArraySum/LongestSubSumArrayLength.java
+++ src/com/company/SubArraySum/LongestSubSumArrayLength.java
@@ -1,63 +1,32 @@
 package com.company.SubArraySum;
+
+import java.util.HashMap;
+
 /*
-给定一个   正整数   组成的无序数组arr，给定一个正整数值K
+给定一个整数组成的无序数组arr，值可能正、可能负、可能0
+给定一个整数值K
 找到arr的所有子数组里，哪个子数组的累加和等于K，并且是长度最大的
 返回其长度
 */
-public class LongestSubSumArrayLength {
+public class LongestSubSumArrayLength{
     public static void main(String[] args) {
-//        System.out.println(getMaxLength(new int[]{1, 2, 3, 7, 1, 1, 1, 1, 1, 1, 1}, 7));
-        System.out.println(getSubArraylength(new int[]{1, 2, 3, 7, 1, 1, 1, 1, 1, 1, 1}, 7));
+        System.out.println(LongestSubArrayLength(new int[]{-1, -2, 3, 7, 1, 1, 1, 1, 1, 1, 1, -1, 1}, 7));
+    }
+    public static int LongestSubArrayLength(int[]arr,int target){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,-1);
+        int len = 0;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if(!map.containsKey(sum)){
+                map.put(sum,i);
+            }
+            if(map.containsKey(sum - target)){
+                len = Math.max(len,i - map.get(sum - target));
+            }
+        }
+        return len;
     }
 
-    public static int getSubArraylength(int[]arr, int target){
-        int start = 0;
-        int end = 0;
-        int len = 0;
-        int sum = arr[0];
-        while(end < arr.length){
-            if(sum < target){
-                end++;
-                if(end >= arr.length){
-                    break;
-                }
-                sum += arr[end];
-            }else if(sum > target){
-                sum -= arr[start++];
-            }else if(sum == target){
-                len = Math.max(len,end - start + 1);
-                sum -= arr[start++];
-//                if(++end == arr.length){
-//                    break;
-//                }
-//                sum +=arr[end];
-            }
-        }
-        return len;
-    }
-    //standard
-    public static int getMaxLength(int[] arr, int K) {
-        if (arr == null || arr.length == 0 || K <= 0) {
-            return 0;
-        }
-        int left = 0;
-        int right = 0;
-        int sum = arr[0];
-        int len = 0;
-        while (right < arr.length) {
-            if (sum == K) {
-                len = Math.max(len, right - left + 1);
-                sum -= arr[left++];
-            } else if (sum < K) {
-                right++;
-                if (right == arr.length) {
-                    break;
-                }
-                sum += arr[right];
-            } else {
-                sum -= arr[left++];
-            }
-        }
-        return len;
-    }
 }
