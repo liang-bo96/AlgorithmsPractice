ce736e5e60720cc1a05e7156e4d0e5dc62aebcff
liang-bo96 <1304617624@qq.com>
2021-06-17 12:30:10 +0800

    6-17

---
+++ src/com/company/DoublePointer/N42_trap_rain_water.java
@@ -0,0 +1,64 @@
+package com.company.DoublePointer;
+
+
+/*
+* 42. 接雨水
+给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+
+
+
+示例 1：
+
+输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+输出：6
+解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+示例 2：
+
+输入：height = [4,2,0,3,2,5]
+输出：9*/
+public class N42_trap_rain_water {
+    public static void main(String[] args) {
+        System.out.println(new N42_trap_rain_water().trap2(new int[]{4,2,0,3,2,5}));
+    }
+    //i位置的水柱高度等于min(max(左边足底啊高度),max(右边最大高度)) - height[i]
+    public int trap(int[] height) {
+        int result = 0;
+        for(int i = 1; i < height.length; i++){
+            int leftheight = 0;
+            for(int m = 0; m < i;m++){
+                if(height[m] > leftheight){
+                    leftheight = height[m];
+                }
+            }
+            int rightheight = 0;
+            for(int n = i + 1; n < height.length; n++){
+                if(height[n] > rightheight){
+                    rightheight = height[n];
+                }
+            }
+            result += Math.max((Math.min(leftheight, rightheight) - height[i]), 0);
+        }
+        return result;
+    }
+    //将左右最大值用数组缓存
+    public int trap2(int[] height) {
+        int result = 0;
+        int[] left = new int[height.length];
+        int[] right = new int[height.length];
+        for(int i = 1; i < left.length; i++){
+            left[i] = Math.max(height[i-1],left[i-1]);
+        }
+        for(int i = right.length - 2; i >= 0 ;i--){
+            right[i] = Math.max(height[i+1],right[i+1]);
+        }
+        for(int i = 1; i < height.length; i++){
+            result += (Math.min(left[i],right[i]) - height[i]) > 0
+                    ? Math.min(left[i],right[i]) - height[i] : 0;
+        }
+        return result;
+    }
+
+
+
+}
+
---
+++ src/com/company/DoublePointer/N75_sort_color.java
@@ -0,0 +1,59 @@
+package com.company.DoublePointer;
+
+import static com.company.Leetcode.N46.swap;
+
+/*
+* 75. 颜色分类
+给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+
+此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+
+示例 1：
+
+输入：nums = [2,0,2,1,1,0]
+输出：[0,0,1,1,2,2]
+示例 2：
+
+输入：nums = [2,0,1]
+输出：[0,1,2]
+示例 3：
+
+输入：nums = [0]
+输出：[0]
+示例 4：
+
+输入：nums = [1]
+输出：[1]
+
+提示：
+
+n == nums.length
+1 <= n <= 300
+nums[i] 为 0、1 或 2
+* */
+public class N75_sort_color {
+    public static void main(String[] args) {
+        new N75_sort_color().sortColors(new int[]{2,0,1});
+    }
+    public void sortColors(int[] nums) {
+        if(nums.length == 1) return;
+        int left = 0;
+        int right = nums.length - 1;
+
+        for (int i = 0; i < nums.length ; i++){
+            while(nums[i] == 2 && right >=i){
+                swap(nums,i,right);
+                right--;
+            }
+            if(nums[i] == 0){
+                swap(nums,i,left);
+                left++;
+            }
+        }
+    }
+    public static void swap(int[] nums,int i, int j){
+        int tem = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tem;
+    }
+}
