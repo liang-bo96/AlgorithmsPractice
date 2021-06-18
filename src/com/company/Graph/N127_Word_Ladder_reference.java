4b7f93da02a6d1975855b7f0015746ca23dee267
liang-bo96 <67692596+liang-bo96@users.noreply.github.com>
2021-06-18 21:10:09 +0800

    6-18

--- src/com/company/Graph/N127_Word_Ladder_reference.java
+++ src/com/company/Graph/N127_Word_Ladder_reference.java
@@ -35,62 +35,74 @@ beginWord、endWord 和 wordList[i] 由小写英文字母组成
 beginWord != endWord
 wordList 中的所有字符串 互不相同*/
 public class N127_Word_Ladder_reference {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        // 第 1 步：先将 wordList 放到哈希表里，便于判断某个单词是否在 wordList 里
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (wordSet.size() == 0 || !wordSet.contains(endWord)) {
+            return 0;
+        }
+        wordSet.remove(beginWord);
 
-        Map<String, Integer> wordId = new HashMap<String, Integer>();
-        List<List<Integer>> edge = new ArrayList<List<Integer>>();
-        int nodeNum = 0;
+        // 第 2 步：图的广度优先遍历，必须使用队列和表示是否访问过的 visited 哈希表
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        Set<String> visited = new HashSet<>();
+        visited.add(beginWord);
 
-        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-            for (String word : wordList) {
-                addEdge(word);
-            }
-            addEdge(beginWord);
-            if (!wordId.containsKey(endWord)) {
-                return 0;
-            }
-            int[] dis = new int[nodeNum];
-            Arrays.fill(dis, Integer.MAX_VALUE);
-            int beginId = wordId.get(beginWord), endId = wordId.get(endWord);
-            dis[beginId] = 0;
-
-            Queue<Integer> que = new LinkedList<Integer>();
-            que.offer(beginId);
-            while (!que.isEmpty()) {
-                int x = que.poll();
-                if (x == endId) {
-                    return dis[endId] / 2 + 1;
+        // 第 3 步：开始广度优先遍历，包含起点，因此初始化的时候步数为 1
+        int step = 1;
+        while (!queue.isEmpty()) {
+            int currentSize = queue.size();
+            for (int i = 0; i < currentSize; i++) {
+                // 依次遍历当前队列中的单词
+                String currentWord = queue.poll();
+                // 如果 currentWord 能够修改 1 个字符与 endWord 相同，则返回 step + 1
+                if (changeWordEveryOneLetter(currentWord, endWord, queue, visited, wordSet)) {
+                    return step + 1;
                 }
-                for (int it : edge.get(x)) {
-                    if (dis[it] == Integer.MAX_VALUE) {
-                        dis[it] = dis[x] + 1;
-                        que.offer(it);
+            }
+            step++;
+        }
+        return 0;
+    }
+
+    /**
+     * 尝试对 currentWord 修改每一个字符，看看是不是能与 endWord 匹配
+     *
+     * @param currentWord
+     * @param endWord
+     * @param queue
+     * @param visited
+     * @param wordSet
+     * @return
+     */
+    private boolean changeWordEveryOneLetter(String currentWord, String endWord,
+                                             Queue<String> queue, Set<String> visited, Set<String> wordSet) {
+        char[] charArray = currentWord.toCharArray();
+        for (int i = 0; i < endWord.length(); i++) {
+            // 先保存，然后恢复
+            char originChar = charArray[i];
+            for (char k = 'a'; k <= 'z'; k++) {
+                if (k == originChar) {
+                    continue;
+                }
+                charArray[i] = k;
+                String nextWord = String.valueOf(charArray);
+                if (wordSet.contains(nextWord)) {
+                    if (nextWord.equals(endWord)) {
+                        return true;
+                    }
+                    if (!visited.contains(nextWord)) {
+                        queue.add(nextWord);
+                        // 注意：添加到队列以后，必须马上标记为已经访问
+                        visited.add(nextWord);
                     }
                 }
             }
-            return 0;
+            // 恢复
+            charArray[i] = originChar;
         }
+        return false;
+    }
 
-        public void addEdge(String word) {
-            addWord(word);
-            int id1 = wordId.get(word);
-            char[] array = word.toCharArray();
-            int length = array.length;
-            for (int i = 0; i < length; ++i) {
-                char tmp = array[i];
-                array[i] = '*';
-                String newWord = new String(array);
-                addWord(newWord);
-                int id2 = wordId.get(newWord);
-                edge.get(id1).add(id2);
-                edge.get(id2).add(id1);
-                array[i] = tmp;
-            }
-        }
-
-        public void addWord(String word) {
-            if (!wordId.containsKey(word)) {
-                wordId.put(word, nodeNum++);
-                edge.add(new ArrayList<Integer>());
-            }
-        }
 }
