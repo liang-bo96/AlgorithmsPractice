494c1d42a2ed6ae454ba2f12596990383222c0bc
liang-bo96 <1304617624@qq.com>
2021-07-11 22:57:53 +0800

    6-26

---
+++ src/com/company/DoubleHeap/N480_sliding_window_median.java
@@ -0,0 +1,30 @@
+package com.company.DoubleHeap;
+
+import java.util.*;
+
+public class N480_sliding_window_median {
+    public static void main(String[] args) {
+        System.out.println(new N480_sliding_window_median().medianSlidingWindow(new int[]{1,2,3,4}, 4));
+    }
+    public double[] medianSlidingWindow(int[] nums, int k) {
+        if(nums == null || k > nums.length) return null;
+        List<Long> list = new LinkedList<>();
+        double[] result = new double[nums.length - k + 1];
+        for (int i = 0; i < k; i++) {
+            list.add((long)nums[i]);
+        }
+        for (int i = k; i < nums.length; i++) {
+            Collections.sort(list);
+            result[i - k] = k % 2 == 0 ? (list.get(k/2) + list.get(k / 2))*0.5:list.get(k /2);
+            for(int j = 0; j < list.size(); j++){
+                if(list.get(j) == nums[i - k]){
+                    list.remove(j);
+                }
+            }
+            list.add((long)nums[i]);
+        }
+        Collections.sort(list);
+        result[nums.length - k ] =k % 2 == 0 ?  (list.get(k/2) + list.get(k / 2 - 1))*0.5:list.get(k /2);
+        return result;
+    }
+}
---
+++ src/com/company/Hot100/List/N1.java
@@ -0,0 +1,4 @@
+package com.company.Hot100.List;
+
+public class N1 {
+}
