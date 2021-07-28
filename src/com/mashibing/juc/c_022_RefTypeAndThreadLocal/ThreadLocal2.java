c88adbe39a685bf007f9bb34d22f83423ea080cb
liang-bo96 <1304617624@qq.com>
2021-07-28 11:47:23 +0800

    JUC

--- src/com/mashibing/juc/c_022_RefTypeAndThreadLocal/ThreadLocal2.java
+++ src/com/mashibing/juc/c_022_RefTypeAndThreadLocal/ThreadLocal2.java
@@ -10,31 +10,33 @@
  */
 package com.mashibing.juc.c_022_RefTypeAndThreadLocal;
 
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 public class ThreadLocal2 {
 	//volatile static Person p = new Person();
-	static ThreadLocal<Person> tl = new ThreadLocal<>();
+	static ThreadLocal<Object> tl = new ThreadLocal<>();
+	static ThreadLocal<Object> tt = new ThreadLocal<>();
 	
 	public static void main(String[] args) {
-				
+
+		new Thread(()->{
+			HashMap<Object,Object> map = new HashMap<>();
+			tl.set("asd");
+			tl.set("asd");
+			tl.set("qwe");
+			tt.set("!@3");
+			System.out.println(tl.get());
+		}).start();
+
 		new Thread(()->{
 			try {
 				TimeUnit.SECONDS.sleep(2);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			
+
 			System.out.println(tl.get());
-		}).start();
-		
-		new Thread(()->{
-			try {
-				TimeUnit.SECONDS.sleep(1);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			tl.set(new Person());
 		}).start();
 	}
 	
